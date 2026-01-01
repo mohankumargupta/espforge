84078a77248997d14e3f4a44e0c4fa431fe2b02c
Mohan Gupta <mohangupta@live.com>
2026-01-01 18:05:18 +1100

    re-architect espforge

--- espforge_platform/src/lib.rs
+++ espforge_platform/src/lib.rs
@@ -1,13 +1,17 @@
-pub mod logger;
+// pub mod logger;
 
-pub struct Context {
-    pub logger: logger::Logger,
-}
+// pub struct Context {
+//     pub logger: logger::Logger,
+// }
 
-impl Context {
-    pub fn new() -> Self {
-        Self {
-            logger: logger::Logger,
-        }
-    }
-}
+// impl Context {
+//     pub fn new() -> Self {
+//         Self {
+//             logger: logger::Logger,
+//         }
+//     }
+// }
+
+use include_dir::{Dir, include_dir};
+
+pub static PLATFORM_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/assets");
