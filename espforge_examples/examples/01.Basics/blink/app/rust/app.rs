a224dd7695a067da9f67fe92567628ece1acfe59
Mohan Gupta <mohangupta@live.com>
2026-01-05 10:00:25 +1100

    major re-architecture

--- espforge_examples/examples/01.Basics/blink/app/rust/app.rs
+++ espforge_examples/examples/01.Basics/blink/app/rust/app.rs
@@ -2,9 +2,12 @@
 use crate::Context;
 
 pub fn setup(ctx: &mut Context) {
-    let logger = ctx.logger;
 }
 
 pub fn forever(ctx: &mut Context) {
+    if let Some(led) = config.component("red_led") {
+        led.toggle();
+    }
+
 
 }
