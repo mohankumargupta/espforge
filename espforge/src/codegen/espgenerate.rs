cebf443366e1093fcde2ffe83e6a342a0fcd9084
Mohan Gupta <mohangupta@live.com>
2026-01-05 14:59:05 +1100

    major re-architecture

--- espforge/src/codegen/espgenerate.rs
+++ espforge/src/codegen/espgenerate.rs
@@ -4,19 +4,15 @@ use std::process::Command;
 pub fn esp_generate(project_name: &str, chip: &str, enable_async: bool) -> Result<()> {
     println!("Running esp-generate for chip: {}", chip);
     let mut cmd = Command::new("esp-generate");
-    cmd.arg("--headless")
-        .arg("--chip")
-        .arg(chip)
-        .arg("-o")
-        .arg("log")
-        .arg("-o")
-        .arg("unstable-hal")
-        .arg("-o")
-        .arg("esp-backtrace")
-        .arg("-o")
-        .arg("wokwi")
-        .arg("-o")
-        .arg("vscode");
+    cmd.args([
+        "--headless", 
+        "--chip", chip,
+        "-o", "log",
+        "-o", "unstable-hal",
+        "-o", "esp-backtrace",
+        "-o", "wokwi",
+        "-o", "vscode",
+    ]);
 
     if enable_async {
         cmd.arg("-o").arg("embassy");
