223a666e566333a7026e0b08d4ffd63389c0e5bb
Mohan Gupta <mohangupta@live.com>
2026-01-06 22:22:44 +1100

    major refactor

--- espforge_examples/src/lib.rs
+++ espforge_examples/src/lib.rs
@@ -1,4 +1,5 @@
 use include_dir::{Dir, include_dir};
 
-// Embeds the 'examples' directory found at the root of this crate
 pub static EXAMPLES_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/examples");
+pub static EXTRA_DEPENDENCIES: &'static str = include_str!("../dependencies.toml");
+
