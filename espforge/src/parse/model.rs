9faac6d62b2816f3d50a7be2bc636100c0e98122
Mohan Gupta <mohangupta@live.com>
2026-01-10 20:11:34 +1100

    major refactor

--- espforge/src/parse/model.rs
+++ espforge/src/parse/model.rs
@@ -1,9 +1,3 @@
-use crate::parse::processor::{ProcessorRegistration, SectionProcessor};
-use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
-use serde_yaml_ng::Value;
-use std::collections::HashMap;
-
 pub use espforge_common::{
     Component, ComponentResource, Esp32Config, GpioPinConfig, I2cConfig, PinDirection,
     ProjectModel, ResourceRef, ResourceResolver, SpiConfig, UartConfig,
