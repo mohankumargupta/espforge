23396ab9873ac194171cf565218926e55a983461
Mohan Gupta <mohangupta@live.com>
2026-01-10 20:18:24 +1100

    major refactor

--- espforge/src/codegen/components.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-use espforge_common::ProjectModel;
-
-pub fn generate_components_source(model: &ProjectModel) -> Result<String> {
-    espforge_codegen::generate_components_source(model)
-}
-
