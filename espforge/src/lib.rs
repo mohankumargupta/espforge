80466230753fd9732dd534bf57360c299644ad5f
Mohan Gupta <mohangupta@live.com>
2026-01-20 12:32:34 +1100

    remove legacy code

--- espforge/src/lib.rs
+++ espforge/src/lib.rs
@@ -3,6 +3,4 @@ pub mod compile;
 pub mod examples;
 pub mod parse;
 
-use include_dir::{Dir, include_dir};
-pub static PLATFORM_SRC: Dir = include_dir!("$CARGO_MANIFEST_DIR/../espforge_platform");
-pub static DEVICES_SRC: Dir = include_dir!("$CARGO_MANIFEST_DIR/../espforge_devices");
+
