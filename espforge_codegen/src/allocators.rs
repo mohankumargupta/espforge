7e80cdacfe32f4b96c45b05552007449d8866fa8
Mohan Gupta <mohangupta@live.com>
2026-06-30 12:19:32 +1000

    steal

--- espforge_codegen/src/allocators.rs
+++ espforge_codegen/src/allocators.rs
@@ -33,7 +33,7 @@ impl AllocatorGenerator {
     fn psram_allocator(model: &EspforgeConfiguration) -> TokenStream {
         if model.has_psram() == Some(true) {
             quote! {
-                esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
+                esp_alloc::psram_allocator!(unsafe { esp_hal::peripherals::PSRAM::steal() }, esp_hal::psram);
             }
         } else {
             TokenStream::new()
