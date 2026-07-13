71aee445193e7942ece40fe4ccb68a74d373b655
Mohan Gupta <raspberrypisig@gmail.com>
2026-07-13 14:28:10 +1000

    porting more examples to v2

---
+++ v2/espforge-examples/examples/01.Basics/helloworld/app/rust/app.rs
@@ -0,0 +1,12 @@
+#[warn(unused_variables)]
+
+use crate::Context;
+
+pub fn setup(ctx: &mut Context) {
+    let logger = ctx.logger;
+    logger.info("Hello World");
+}
+
+pub fn forever(ctx: &mut Context) {
+
+}
