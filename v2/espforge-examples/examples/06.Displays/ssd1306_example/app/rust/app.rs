23bc50ed6ae6429019e5f91d5b5f82363b52679e
Mohan Gupta <raspberrypisig@gmail.com>
2026-07-15 07:11:45 +1000

    fixing display examples

---
+++ v2/espforge-examples/examples/06.Displays/ssd1306_example/app/rust/app.rs
@@ -0,0 +1,23 @@
+#[allow(unused_variables)]
+
+use crate::{Context, device};
+
+pub fn setup(ctx: &mut Context) {
+    let logger = ctx.logger;
+    let oled = device!(ctx, oled);
+
+    logger.info("Initializing OLED...");
+    
+    oled.init();
+    oled.clear();
+    oled.print(0, 0, "Hello Espforge!");
+    oled.flush();
+    
+    logger.info("OLED Initialized");
+}
+
+pub fn forever(ctx: &mut Context) {
+    let delay = ctx.delay;
+    
+    delay.delay_ms(1000);
+}
